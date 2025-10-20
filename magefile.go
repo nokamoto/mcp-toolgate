093c5106c7805b86816052b94fa891813d97665f
nokamoto <nokamoto.engr@gmail.com>
2025-10-20 22:04:02 +0900

    Refactor Format function to use command chain for better error handling

--- magefile.go
+++ magefile.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
@@ -18,15 +20,21 @@ func CI() {
 }
 
 // Format formats the code using goimports and gofumpt.
-func Format() {
-	g0("install", "golang.org/x/tools/cmd/goimports@latest")
-	sh.Run("goimports", "-l", "-w", ".")
-
-	g0("install", "mvdan.cc/gofumpt@latest")
-	sh.Run("gofumpt", "-l", "-w", ".")
-
-	g0("install", "github.com/google/yamlfmt/cmd/yamlfmt@latest")
-	sh.Run("yamlfmt", ".")
+func Format() error {
+	chain := [][]string{
+		{"go", "install", "golang.org/x/tools/cmd/goimports@latest"},
+		{"go", "install", "mvdan.cc/gofumpt@latest"},
+		{"go", "install", "github.com/google/yamlfmt/cmd/yamlfmt@latest"},
+		{"goimports", "-l", "-w", "."},
+		{"gofumpt", "-l", "-w", "."},
+		{"yamlfmt", "."},
+	}
+	for _, c := range chain {
+		if err := sh.RunV(c[0], c[1:]...); err != nil {
+			return fmt.Errorf("failed to run command %v: %w", c, err)
+		}
+	}
+	return nil
 }
 
 // Test runs the tests in the project.
